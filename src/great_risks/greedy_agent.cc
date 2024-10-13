92bec142ec60e8d4648c73262470b5ad6ca42fa8
Andrew Lu <lu987@purdue.edu>
2024-10-13 15:55:35 -0400

    improve greedy agent

--- src/great_risks/greedy_agent.cc
+++ src/great_risks/greedy_agent.cc
@@ -16,7 +16,7 @@ namespace great_risks {
             }
             auto search_result = field.shortest_path({robot_state.x, robot_state.y}, grabbable_goals, robot_state.is_red);
             // if we can grab a goal, grab it
-            if (search_result.second.empty() && std::find(legal_actions.begin(), legal_actions.end(), GRAB_MOBILE_GOAL) != legal_actions.end()) {
+            if (search_result.second.empty() && !grabbable_goals.empty() && std::find(legal_actions.begin(), legal_actions.end(), GRAB_MOBILE_GOAL) != legal_actions.end()) {
                 return GRAB_MOBILE_GOAL;
             }
             if (!search_result.second.empty()) {
@@ -28,10 +28,6 @@ namespace great_risks {
             // if we can score more rings on the goal
             if (std::find(legal_actions.begin(), legal_actions.end(), SCORE_MOBILE_GOAL) != legal_actions.end()) {
                 return SCORE_MOBILE_GOAL;
-            }
-            // if we can score on wall stake
-            if (std::find(legal_actions.begin(), legal_actions.end(), SCORE_WALL_STAKE) != legal_actions.end()) {
-                return SCORE_WALL_STAKE;
             }
             bool left_corner_occupied = false;
             bool right_corner_occupied = false;
@@ -51,35 +47,8 @@ namespace great_risks {
             if (!right_corner_occupied && field.time_remaining > 15) {
                 positive_corners.insert({10, 10});
             }
-            // otherwise if goal can fit more rings pick up rings
-            if (goal.rings.size() < 6 || (positive_corners.empty() && robot_state.rings.size() < 2)) {
-                if (robot_state.is_red && std::find(legal_actions.begin(), legal_actions.end(), PICK_UP_RED) != legal_actions.end()) {
-                    return PICK_UP_RED;
-                }
-                if (!robot_state.is_red && std::find(legal_actions.begin(), legal_actions.end(), PICK_UP_BLUE) != legal_actions.end()) {
-                    return PICK_UP_BLUE;
-                }
-                std::unordered_set<std::array<std::uint8_t, 2>> grabbable_rings;
-                for (uint8_t i = 0; i < 11; i++) {
-                    for (uint8_t j = 0; j < 11; j++) {
-                        std::array<std::uint8_t, 2> ring_pos = {i, j};
-                        if (robot_state.is_red && field.red_rings[i][j]) {
-                            grabbable_rings.insert(ring_pos);
-                        }
-                        else if (!robot_state.is_red && field.blue_rings[i][j]) {
-                            grabbable_rings.insert(ring_pos);
-                        }
-                    }
-                }
-                if (!grabbable_rings.empty()) {
-                    auto search_result = field.shortest_path({robot_state.x, robot_state.y}, grabbable_rings, robot_state.is_red);
-                    if (!search_result.second.empty()) {
-                        return search_result.second[0];
-                    }
-                }
-            }
             // otherwise try to release goal in positive corner
-            if (!positive_corners.empty()) {
+            if (goal.rings.size() == 6 && !positive_corners.empty()) {
                 auto search_result = field.shortest_path({robot_state.x, robot_state.y}, positive_corners, robot_state.is_red);
                 if (!search_result.second.empty()) {
                     return search_result.second[0];
@@ -88,15 +57,46 @@ namespace great_risks {
                     return RELEASE_MOBILE_GOAL;
                 }
             }
-            if (robot_state.rings.size() > 0) {
-                std::unordered_set<std::array<std::uint8_t, 2>> stakes = {
-                    {0, 5},
-                    {10, 5}
-                };
-                auto search_result = field.shortest_path({robot_state.x, robot_state.y}, stakes, robot_state.is_red);
-                if (!search_result.second.empty()) {
-                    return search_result.second[0];
+        }
+        if (robot_state.rings.size() > 0) {
+            std::unordered_set<std::array<std::uint8_t, 2>> stakes;
+            if (field.stakes[0].rings.size() < 6) {
+                stakes.insert({0, 5});
+            }
+            if (field.stakes[1].rings.size() < 6) {
+                stakes.insert({10, 5});
+            }
+            auto search_result = field.shortest_path({robot_state.x, robot_state.y}, stakes, robot_state.is_red);
+            if (!search_result.second.empty()) {
+                return search_result.second[0];
+            }
+        }
+        // if we can score on wall stake
+        if (std::find(legal_actions.begin(), legal_actions.end(), SCORE_WALL_STAKE) != legal_actions.end()) {
+            return SCORE_WALL_STAKE;
+        }
+        if (robot_state.is_red && std::find(legal_actions.begin(), legal_actions.end(), PICK_UP_RED) != legal_actions.end()) {
+            return PICK_UP_RED;
+        }
+        if (!robot_state.is_red && std::find(legal_actions.begin(), legal_actions.end(), PICK_UP_BLUE) != legal_actions.end()) {
+            return PICK_UP_BLUE;
+        }
+        std::unordered_set<std::array<std::uint8_t, 2>> grabbable_rings;
+        for (uint8_t i = 0; i < 11; i++) {
+            for (uint8_t j = 0; j < 11; j++) {
+                std::array<std::uint8_t, 2> ring_pos = {i, j};
+                if (robot_state.is_red && field.red_rings[i][j]) {
+                    grabbable_rings.insert(ring_pos);
                 }
+                else if (!robot_state.is_red && field.blue_rings[i][j]) {
+                    grabbable_rings.insert(ring_pos);
+                }
+            }
+        }
+        if (!grabbable_rings.empty()) {
+            auto search_result = field.shortest_path({robot_state.x, robot_state.y}, grabbable_rings, robot_state.is_red);
+            if (!search_result.second.empty()) {
+                return search_result.second[0];
             }
         }
         return legal_actions[0];
